e55d6e9cd63fcb5f6a9c2bf2d7a10c460e8dd445
idan <idan@moveo.co.il>
2018-02-25 23:25:25 +0200

    fucking fuck balls

--- src/com/shenkar/hibernate/controller/ToDoProgramController.java
+++ src/com/shenkar/hibernate/controller/ToDoProgramController.java
@@ -26,7 +26,7 @@ import com.shenkar.hibernate.model.UserExceptionHandler;
 
 @WebServlet("/usercontroller/*")
 public class ToDoProgramController extends HttpServlet {
-	
+	//whattttttttttttttttttttt
 	private static final long serialVersionUID = 1L;
     private HttpSession session; 
     private RequestDispatcher dispatcher;
